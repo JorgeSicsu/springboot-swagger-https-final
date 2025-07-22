48c2f72f3cc2c8c246749f3a8fdc284c5624706c
JorgeSicsu <jlsicsu@hotmail.com>
2025-07-22 15:42:01 -0300

    Teste de Http para Https

---
+++ src/main/java/com/example/demo/HttpsRedirectConfig.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpsRedirectConfig {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainerCustomizer() {
+        return server -> server.addConnectorCustomizers(connector -> {
+            connector.setScheme("http");
+            connector.setSecure(false);
+            connector.setRedirectPort(443);
+        });
+    }
+}
